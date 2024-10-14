5b2b092d958d1aed15d37d4d1d71350a9bf1a618
Daniil Zhuravlev <nakrovati@outlook.com>
2024-10-15 01:25:07 +0400

    chore: replace Bun with Node
    There are a few bugs when working with Bun, like missing import.meta.env
    variables or bugs with @vite-pwa/astro.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,15 @@ import { defineConfig } from "astro/config";
 
 import { manifest } from "./src/utils/manifest.ts";
 
+const isDev = import.meta.env.DEV;
+
 // https://astro.build/config
 export default defineConfig({
   adapter: vercel({ webAnalytics: { enabled: true } }),
   integrations: [
     tailwind(),
     AstroPWA({
+      devOptions: { enabled: isDev },
       manifest,
       registerType: "autoUpdate",
     }),
